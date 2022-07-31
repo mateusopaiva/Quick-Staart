107ca6d1de2dc6a5561c4624d59161766def8672
Mateus Paiva <mateusopaiva@gmail.com>
2022-07-31 16:49:19 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function revelarResposta() {
+    var resposta = document.querySelector("#resposta");
+    resposta.classList.toggle("blur");
+}
+
+function proximaPergunta(proximaPergunta) {
+    var pergunta = document.querySelector("#cardContainer");
+    pergunta.innerHTML = "";
+
+    var cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "animate__animated", "animate__backInRight");
+    cardDiv.innerHTML = `
+    <div class="card-cabecalho centralizar">
+        <!--Pergunta-->
+        <h1 class="card-titulo">O que é ${proximaPergunta.title}?</h1>
+    </div>
+    <div id="resposta" class="card-conteudo blur">
+        <!--Resposta-->
+        <p>${proximaPergunta.description}</p>
+    </div>
+    `;
+    pergunta.appendChild(cardDiv);
+}
+
+function buscarInformacao() {
+    fetch("https://flash.quickstaart.com/random")
+    .then(function (resultado) {
+        return resultado.json();
+    })
+    .then(function (resultadoJson) {
+        proximaPergunta(resultadoJson);
+});
+}
+
+window.addEventListener("load", buscarInformacao);
