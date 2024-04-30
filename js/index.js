441497ac8230fecdf11bd17ab0f88cf083a2d485
Mateus Paiva <mateusopaiva@gmail.com>
2024-04-30 15:26:53 -0300

    Refatorando o index.js

--- js/index.js
+++ js/index.js
@@ -1,46 +1,13 @@
 import { questions } from "./questions.js";
 
-var revelarResposta = document.querySelector('#revelarResposta')
-revelarResposta.onclick = function revelarResposta() {
+var revelarRespostaButton = document.querySelector('#revelarResposta');
+var atualizarPerguntaButton = document.querySelector('#atualizarPergunta');
+var cardContainer = document.querySelector("#cardContainer");
+
+function toggleBlur() {
     var resposta = document.querySelector("#resposta");
     resposta.classList.toggle("blur");
 }
-
-var atualizarDadosPergunta = document.querySelector('#atualizarPergunta')
-atualizarDadosPergunta.onclick = buscarInformacao
-
-function proximaPergunta(proximaPergunta) {
-    var pergunta = document.querySelector("#cardContainer");
-    pergunta.innerHTML = "";
-
-    var cardDiv = document.createElement("div");
-    cardDiv.classList.add("card", "animate__animated", "animate__backInRight");
-    cardDiv.innerHTML = `
-    <div class="card-cabecalho centralizar">
-        <!--Pergunta-->
-        <h1 class="card-titulo">${proximaPergunta.title}?</h1>
-    </div>
-    <div id="resposta" class="card-conteudo blur">
-        <!--Resposta-->
-        <p>${proximaPergunta.description}</p>
-    </div>
-    `;
-    pergunta.appendChild(cardDiv);
-}
-
-/*
-function buscarInformacao() {
-    fetch("https://flash.quickstaart.com/random")
-    .then(function (resultado) {
-        return resultado.json();
-    })
-    .then(function (resultadoJson) {
-        proximaPergunta(resultadoJson);
-    });
-} 
-
-window.addEventListener('load', buscarInformacao)
-*/
 
 function buscarInformacao() {
     var valorAleatorio = Math.floor(Math.random() * questions.length);
@@ -48,4 +15,25 @@ function buscarInformacao() {
     proximaPergunta(perguntaAleatoria);
 }
 
-buscarInformacao();
+function proximaPergunta(proximaPergunta) {
+    cardContainer.innerHTML = "";
+
+    var cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "animate__animated", "animate__backInRight");
+
+    cardDiv.innerHTML = `
+        <div class="card-cabecalho centralizar">
+            <h1 class="card-titulo">${proximaPergunta.title}?</h1>
+        </div>
+        <div id="resposta" class="card-conteudo blur">
+            <p>${proximaPergunta.description}</p>
+        </div>
+    `;
+
+    cardContainer.appendChild(cardDiv);
+}
+
+revelarRespostaButton.addEventListener("click", toggleBlur);
+atualizarPerguntaButton.addEventListener("click", buscarInformacao);
+
+window.addEventListener("load", buscarInformacao);
